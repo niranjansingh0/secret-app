315cd42a660993ea3d8b9c754bec2ae679602d15
niranjansingh0 <niranjan2210018-d@akgec.ac.in>
2023-10-21 23:25:34 +0530

    first commit

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets"
+  callbackURL: "https://secret-ph3p.onrender.com/auth/google/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
   console.log(profile);
